f098773d6769f84997b7158e5bd378749ea69fc9
Wahhaj Javed <14081651+wahhajjaved@users.noreply.github.com>
2025-03-09 11:25:08 -0600

    added line moving and cloning

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
 local opt = vim.opt
+local keymap = vim.keymap.set
+local km_ops = {noremap = true, silent = true}
 
-vim.cmd("let g:netrw_liststyle = 3") -- so built in file explorer uses tree list style
+
+-------------------
+----- Options -----
+-------------------
 
 opt.relativenumber = true
 opt.number = true
@@ -21,10 +26,57 @@ vim.cmd [[colorscheme onedark]]
 
 opt.backspace = "indent,eol,start"
 
-opt.clipboard:append("unnamedplus") -- use system clipboard as default register
-
 opt.splitright = true
 opt.splitbelow = true
+
+
+
+--------------------
+------ Keymaps------
+--------------------
+
+keymap("v", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+
+--paste without yanking
+keymap("v", "p", '"_dP', opts)
+
+-- keep selection after indenting
+keymap("x", "<", "<gv", km_opts)
+keymap("x", ">", ">gv", km_opts)
+
+-- move text up and down
+keymap("n", "<C-j>", ":m .+1<CR>", km_opts)
+keymap("n", "<C-k>", ":m .-2<CR>", km_opts)
+keymap("x", "<C-j>", ":m '>+1<CR>gv=gv", km_opts)
+keymap("x", "<C-k>", ":m '<-2<CR>gv=gv", km_opts)
+
+-- duplicate lines
+keymap("n", "<A-j>", ":t .<CR>", km_opts)
+keymap("n", "<A-k>", ":t .-1<CR>", km_opts)
+keymap("x", "<A-k>", ":t '><CR>gv=gv", km_opts)
+keymap("x", "<A-j>", ":t '<-1<CR>gv=gv", km_opts)
+
+
+-- built in completion
+opt.completeopt = { "menu", "menuone", "noinsert" }
+keymap("i", "<Tab>", "pumvisible() ? '<C-y>' : '<Tab>'", {expr = true, noremap = true})
+keymap("i", "j", "pumvisible() ? '<C-n>' : 'j'", {expr = true, noremap = true})
+keymap("i", "k", "pumvisible() ? '<C-p>' : 'k'", {expr = true, noremap = true})
+keymap("i", "<Esc>", "pumvisible() ? '<C-e>' : '<Esc>'", { expr = true, noremap = true })
+keymap("i", "<Space>", "pumvisible() ? '<C-e> ' : ' '", { expr = true, noremap = true })
+
+
+-- File explorer
+vim.cmd("let g:netrw_liststyle = 3") -- so built in file explorer uses tree list style
+keymap("n", "<F2>", ":if exists('g:loaded_netrw') && &filetype == 'netrw' | bd | else | :Ex | endif<CR>", { noremap = true, silent = true })
+
+-- Splits
+keymap("n", "<Leader>l", "<C-w>l", km_opts)
+keymap("n", "<Leader>h", "<C-w>h", km_opts)
+
+
+
 
 
 
@@ -33,4 +85,15 @@ require("plugin_configs.treesitter")
 
 -- load language servers
 require("ls_configs.clangd")
+
+
+-- custom file types
+vim.filetype.add({
+  extension = {
+    pde = "java",
+  },
+})
+
+
+
 
